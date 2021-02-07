897a4b0d507d2129d6f7f80cfb75a05c97bfa79d
iodar <granzowdario@gmail.com>
2021-02-07 02:15:36 +0100

    feature: added module to request status information from shelly button

---
+++ button-request.ts
@@ -0,0 +1,33 @@
+import fetch from "node-fetch";
+
+/**
+ * Ausschnitt aus Antwort des Shelly Button1 des Endpunkts
+ * `/status` die nur den Status der Batterie enthält.
+ */
+interface ButtonBatteryStatus {
+    bat: {
+        value: number
+        voltage: number
+    }
+}
+
+/**
+ * Sendet eine Anfragen an `/status` des Shelly Button1 dessen IP übergeben
+ * wurde.
+ * @param ipAddress Ip Adresse des Shelly Button an den die Anfrage geschickt werden soll
+ */
+export async function requestButtonStatus(ipAddress: string): Promise<ButtonBatteryStatus> {
+    const statusPath = "/status"
+    const response = await request({ipAddress, path: statusPath}) 
+    return response.json()
+}
+
+interface RequestParams {
+    ipAddress: string,
+    path: string
+}
+
+function request({ipAddress, path}: RequestParams) {
+    const fullUrl = `http://${ipAddress}${path}`
+    return fetch(fullUrl)
+}
